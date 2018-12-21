353c994ea6ebd380ef06e6afa45e29c4a9f745f5
first-name second-nameIngenzi <email@example.comingenziass@gmail.com>
2018-12-21 16:22:33 +0200

    introduction of README

--- js/script.js
+++ js/script.js
@@ -1,55 +1,20 @@
-function mytest()
-{
-var q1=document.forms["myform"]["answer1"].value;
-var q2=document.forms["myform"]["answer2"].value;
-var q3=document.forms["myform"]["answer3"].value;
-var q4=document.forms["myform"]["answer4"].value;
-var q5=document.forms["myform"]["answer5"].value;
-var q6=document.forms["myform"]["answer6"].value;
-var q7=document.forms["myform"]["answer7"].value;
-var q8=document.forms["myform"]["answer8"].value;
-var q9=document.forms["myform"]["answer9"].value;
-var q10=document.forms["myform"]["answer10"].value;
-var marks=0;
-if(q1=="script")
-{
- marks=marks+2;
- }
-if(q2=="Hypertext markup language")
-{
- marks=marks+2;
- }
-if(q3=="The head section")
-{
- marks=marks+2;
-}
-if(q4=="Microsoft")
-{
- marks=marks+2;
-}
-if(q5=="true")
-{
- marks=marks+2;
-}
-if(q6=="alertBox Hello World")
-{
- marks=marks+2;
- }
-if(q7=="function = myFunction()")
-{
- marks=marks+2;
- }
-if(q8=="call myFunction()")
-{
- marks=marks+2;
-}
-if(q9=="if i = 5")
-{
- marks=marks+2;
-}
-if(q10==" if (i != 5)")
-{
- marks=marks+2;
-}
-alert("your score is:"+marks);
+function myTest() {
+    var q1 = document.form["myform"]["optradio1"].value;
+    var q2 = document.form["myform"]["optradio2"].value;
+    var q3 = document.form["myform"]["optradio3"].value;
+    var q4 = document.form["myform"]["optradio4"].value;
+    var marks = 0;
+    if (q1 == "An object-oriented computer programming language commonly used to create interactive effects within web browsers.") {
+        marks = marks + 5;
+    }
+    if (q2 == "Netscape") {
+        marks = marks + 5;
+    }
+    if (q3 == "None") {
+        marks = marks + 5;
+    }
+    if (q4 == "The delete keyword") {
+
+    }
+    alert("your score is:" + marks);
 }
