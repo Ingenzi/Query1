affdd921318c5a59dc509c2f2cf6af71c5c9a9cf
first-name second-nameIngenzi <email@example.comingenziass@gmail.com>
2019-01-07 16:38:58 +0200

    style span

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function myTest() {
+/*function myTest() {
     var q1 = document.form["myform"]["optradio1"].val();
     var q2 = document.form["myform"]["optradio2"].val();
     var q3 = document.form["myform"]["optradio3"].val();
@@ -19,3 +19,18 @@ function myTest() {
     alert("your score is:" + marks)
     event.preventDefault();
 }
+*/
+$(document).ready(function (){
+    $('#blanks form').submit(function(event){
+    var marks=0;
+      var blanks=["choice1", "choice2","choice3","choice4"]
+        blanks.forEach(function(blank) {
+            var userInput = $("input:radio[name=" + blank + "]:checked" ).val();
+                    if(userInput=="B"){
+                    marks = marks+10;
+                    }
+                })
+          alert(marks)
+    event.preventDefault();
+    });
+    });
