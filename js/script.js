84a0c0162f3fde5350ab9484c82a2009d5f6b49e
first-name second-nameIngenzi <email@example.comingenziass@gmail.com>
2019-01-08 18:44:05 +0200

    add font size in css

--- js/script.js
+++ js/script.js
@@ -1,61 +1,25 @@
-        $(document).ready(function(){
+$(document).ready(function(){
 
-            $("form").submit(function(event){
-                event.preventDefault();
-                var optradio1 = $("input[type='radio'][name='answer1']:checked").val();
-                // alert(optradio1);
-                var optradio2 = $("input[type='radio'][name='answer2']:checked").val();
-                
-                var optradio3 = $("input:radio[name=answer3]:checked").val();
-                var optradio4 = $("input:radio[name=answer4]:checked").val();
-                var marks = [optradio1, optradio2, optradio3, optradio4]
-                var score= calculateScore(marks);
-                $(".final").show();
-                $("#display").text("Your score is:" + calculateScore());
-              });
-        //   $("#blanks form").submit(function(event){
-        //     console.log(event);
-            
-        //     // var optradio1 = $("input[type:'radio'][name='answer1']:checked").val();
-        //     // var optradio2 = $("input:radio[name=answer2]:checked").val();
-        //     // var optradio3 = $("input:radio[name=answer3]:checked").val();
-        //     // var optradio4 = $("input:radio[name=answer4]:checked").val();
-        //     // var marks = [optradio1,optradio2,optradio3,optradio4]
-        //     // var score= calculateScore(marks);
-      
-            
-      
-          
-        //     $(".final").show();
-        //     $("#display").text("Your score is:" + marks);
-           
+    $("form").submit(function(event){
+       
+       
+        var optradio1 = parseInt($("input[type='radio'][name='answer1']:checked").val());
+        // alert(optradio1);
+        var optradio2 = parseInt($("input[type='radio'][name='answer2']:checked").val());
+        
+        var optradio3 = parseInt($("input:radio[name=answer3]:checked").val());
+        var optradio4 = parseInt($("input:radio[name=answer4]:checked").val());
 
-            
-        //   })
-        });
+        var userInputs = [optradio1, optradio2, optradio3, optradio4];
+        var score = 0;
+        for (var i = 0; i < userInputs.length; i++){
+            score += userInputs[i];
+        }
+        
+        $(".final").show();
+        $(".z").fadeOut();
+        $("#display").text("Your score is:" + score+"/20");
+        event.preventDefault();
+      });
 
-       function calculateScore(marks){
-        //    var otradio1=answers["0"]
-        //    var otradio2=answers[1]
-        //    var otradio3=answers[2]
-        //    var otradio4=answers[3]
-           console.log(marks["1"]);
-        //    var marks=0;
-        //    if(otradio1=="5")
-        //    {
-        //        marks=marks+5;
-        //    }
-        //    if(otradio2=="5")
-        //    {
-        //        marks=marks+5;
-        //    }
-        //    if(otradio3=="5")
-        //    {
-        //        marks=marks+5;
-        //    }
-        //    if(otradio4=="5")
-        //    {
-        //        marks=marks+5;
-        //    }
-        //    return marks;
-       }
+});
